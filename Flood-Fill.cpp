9b75a0a2a9b753b0fddcdba77b788e8b352a518f
neelima-singh07 <neelima.arun2007@gmail.com>
2025-06-07 08:31:04 -0700

    Update Flood-Fill.cpp

--- Flood-Fill.cpp
+++ Flood-Fill.cpp
@@ -59,3 +59,50 @@ public:
         
     }
 };
+
+
+
+//using BFS Traversal 
+class Solution {
+public:
+    void dfs(vector<vector<int>>&grid,int row,int col,int color,int initial,vector<vector<int>>&vis){
+        grid[row][col]=color;
+        vis[row][col]=1;
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        int n=grid.size();
+        int m=grid[0].size();
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=r+delrow[i];
+                int ncol=c+delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==initial && vis[nrow][ncol]!=1){
+                    grid[nrow][ncol]=color;
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
+            }
+        }
+        
+        
+        
+
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<int>>grid=image;
+        int initial=grid[sr][sc];
+        if (initial == color) return image;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        dfs(grid,sr,sc,color,initial,vis);
+        return grid;
+        
+        
+    }
+};
